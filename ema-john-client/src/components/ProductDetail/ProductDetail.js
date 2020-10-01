ea91f7f299886f0a5f7901a9e9852dd8553bb0fa
Tazwar <tazwar9t6@gmail.com>
2020-10-01 22:14:08 +0600

    removing fake data

--- ema-john-client/src/components/ProductDetail/ProductDetail.js
+++ ema-john-client/src/components/ProductDetail/ProductDetail.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import fakeData from '../../fakeData';
-import Product from '../Product/Product';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Product from "../Product/Product";
 
 const ProductDetail = () => {
-    const {productKey} = useParams();
-    const product = fakeData.find(pd => pd.key === productKey);
-    
-    return (
-        <div>
-            <h1>Your Product Details.</h1>
-            <Product showAddToCart={false} product={product}></Product>
-        </div>
-    );
+  const { productKey } = useParams();
+  let [product, setProduct] = useState({});
+  useEffect(() => {
+    fetch("http://localhost:5000/product/" + productKey)
+      .then((res) => res.json())
+      .then((result) => setProduct(result));
+  }, [productKey]);
+
+  return (
+    <div>
+      <h1>Your Product Details.</h1>
+      <Product showAddToCart={false} product={product}></Product>
+    </div>
+  );
 };
 
 export default ProductDetail;
--- ema-john-server/index.js
+++ ema-john-server/index.js
@@ -1,26 +1,48 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
 const app = express();
+
+const uri =
+  "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/ema-john?retryWrites=true&w=majority";
 app.use(cors());
 app.use(bodyParser.json());
 const port = 5000;
 
-const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dupvj.mongodb.net/db-name?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(
+  uri,
+  { useUnifiedTopology: true },
+  { useNewUrlParser: true },
+  { connectTimeoutMS: 30000 },
+  { keepAlive: 1 }
+);
+
 client.connect((err) => {
   const collection = client.db("ema-john").collection("products");
   // perform actions on the collection object
   // console.log("database connected");
   app.post("/addProduct", (req, res) => {
-    const product = req.body;
-    collection.insertOne(product).then((result) => {
-      console.log(result);
+    const products = req.body;
+    // console.log(products);
+    // console.log("database connected");
+    collection.insertMany(products).then((result) => {
+      res.send(result.insertedCount);
     });
+  });
+
+  app.get("/products", (req, res) => {
+    collection
+      .find({})
+
+      .toArray((err, documents) => res.send(documents));
+  });
+  app.get("/product/:key", (req, res) => {
+    collection
+      .find({ key: req.params.key })
+
+      .toArray((err, documents) => res.send(documents[0]));
   });
 });
 
